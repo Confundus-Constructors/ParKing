1694368204ff75c467412f6357b0b7530b9ce4e7
Kurt Vardeman <kurtvardeman@gmail.com>
2023-09-15 15:52:31 -0400

    set up initial repo

---
+++ exampleData/valet_companies.js
@@ -0,0 +1,14 @@
+companies = [
+  { id: 1,
+    name: 'Mr. Valet',
+    admin_email: 'user@email.com',
+    admin_password: '273498werhwf9ysdf@#$@8f',
+  },
+  { id: 2,
+    name: 'Valet Queen',
+    admin_email: 'user2@email.com',
+    admin_password: 'sadflkjasjdf0848yrt93h4tn3###',
+  },
+];
+
+module.exports = companies;
---
+++ exampleData/vehicles.js
@@ -0,0 +1,23 @@
+// note - user 1 has two vehicles, user 2 has 0
+vehicles = [
+  { id: 1,
+    make: 'Chrystler',
+    color: 'red',
+    license_plate: 'BESTBOSS',
+    user_id: 3,
+  },
+  { id: 2,
+    make: 'Ford',
+    color: 'black',
+    license_plate: 'ABCD123',
+    user_id: 1,
+  },
+  { id: 3,
+    make: 'Toyota',
+    color: 'blue',
+    license_plate: 'DCBA321',
+    user_id: 1,
+  },
+];
+
+module.exports = vehicles;
