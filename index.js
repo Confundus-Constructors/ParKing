dccba90af4d8ae28366541eccb78d0180da3a99c
Kurt Vardeman <kurtvardeman@gmail.com>
2023-09-15 18:41:05 -0400

    edited comments as test

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ require('dotenv').config();
 // const userRoute = require('./routes/users');
 // const restRouter = require('./routes/transactions.js');
 
-// app.use(express.static(path.join(__dirname, "../public")));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
-
-// app.use('/users', userRoute);
-// app.use('/transactions', transactionsRoute);
 
 const port = process.env.SERVER_PORT || 3001;
 
