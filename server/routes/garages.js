ea6e014976e86045554402857b4c5f77c1fcccf2
Kurt Vardeman <kurtvardeman@gmail.com>
2023-09-16 17:26:18 -0400

    cleaned up code. potential bug to fix where I don't actually need to subtract the currently taken spots

--- server/routes/garages.js
+++ server/routes/garages.js
@@ -8,27 +8,24 @@ garageRouter.get('/', async (req, res) => {
   // technically should read req.query for address
     // then call API to convert address to lat / long
     // then use lat / long to query garages and sort by distance
-  // const obj = req.query
-  const obj = {
-    location: '123 ABC St',
-    start_date: "2023-09-17 02:24:00",
-    end_date: "2023-09-17 04:24:00",
-  }
+  const obj = req.query;
+  // const obj = {
+  //   location: '123 ABC St',
+  //   start_date: "2023-09-17 02:24:00",
+  //   end_date: "2023-09-17 04:24:00",
+  // }
 
-  // add number of spots available
-  const garageData = await model.queryAll('garages'); //[ {garage}, {garage} ]
+  const garageData = await model.queryAll('garages');
+
+  // get reservation data
   const transactionCount = await model.queryCountReservationTimes(obj.start_date, obj.end_date);
-  const parkingSpotCount = await model.queryCountParkingSpots();
+  const parkingSpotCount = await model.queryCountParkingSpots(); // is this necessary? it's a screenshot of what's happening now....
 
-  const availableSpots = controller.subtractReservedSpots(parkingSpotCount.rows, transactionCount.rows);
+  // subtract reserved spots from available spots
+  const availableSpots = controller.subtractReservedSpots(parkingSpotCount.rows, transactionCount.rows); // probably not necessary and will take out later
+
+  // nest into garage obj
   const result = controller.nestCountIntoGarageData(garageData.rows, availableSpots);
-  // grab all transactions that fall between dates
-  // subtract count of transactions from parking_spots is_available
-  // append count to each record
-
-  const garageDatarows = garageData.rows;
-  const transactionCountrows = transactionCount.rows;
-  const parkingSpotCountrows = parkingSpotCount.rows;
 
   res.status(201).send(result);
 });
