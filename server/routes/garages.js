c55ca602bc0e34b4d6ae2f076cf6a91952230b3f
Kurt Vardeman <kurtvardeman@gmail.com>
2023-09-18 10:45:09 -0400

    removed console logs and second server file that shouldn't have been there

--- server/routes/garages.js
+++ server/routes/garages.js
@@ -9,23 +9,21 @@ garageRouter.get('/', async (req, res) => {
     // then call API to convert address to lat / long
     // then use lat / long to query garages and sort by distance
   const obj = req.query;
-  console.log({obj});
+  // console.log({obj});
   // const obj = {
   //   location: '123 ABC St',
   //   start_date: "2023-09-17 02:24:00",
   //   end_date: "2023-09-17 04:24:00",
   // }
 
+  // get garage data, parking spot data, reservation data
   const garageData = await model.queryAll('garages');
-
-  // get reservation data
   const transactionCount = await model.queryCountReservationTimes(obj.start_date, obj.end_date);
   const parkingSpotCount = await model.queryCountParkingSpots();
 
-  // get garage data, parking spot data, reservation data
-  console.log('garageData - ', garageData.rows);
-  console.log('transCount - ', transactionCount.rows);
-  console.log('parkingSpotCount - ', parkingSpotCount.rows);
+  // console.log('garageData - ', garageData.rows);
+  // console.log('transCount - ', transactionCount.rows);
+  // console.log('parkingSpotCount - ', parkingSpotCount.rows);
 
   // subtract reserved spots from available spots
   const availableSpots = controller.subtractReservedSpots(parkingSpotCount.rows, transactionCount.rows);
