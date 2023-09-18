0c6cc3b923d6400e75aa518c575c97f9ccb9d296
Kurt Vardeman <kurtvardeman@gmail.com>
2023-09-18 13:15:26 -0400

    working implementation for scanning qr code routes

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -1,34 +1,59 @@
 const express = require('express');
 const transactionRouter = express.Router();
 const path = require('path');
-// const model = require('../models');
+const model = require('../models');
 // const controller = require('../controllers');
 
 transactionRouter.get('/:qr_code', async (req, res) => {
-  const qr_code = req.params;
+  try {
+    const qr_code = req.params.qr_code;
 
-  // transactions
-    // reservation_id
-    // reservation start
-    // reservation end
+    // query to find if it's checked-in
+    const status = await model.queryReservationStatus(qr_code);
+    const current_status = status.rows[0].current_status;
+    console.log(current_status);
 
-  // users
-    // owner
+    // checking in
+    if (current_status === 'reserved') {
+      console.log('checking in');
+      const data = await model.queryReservationUponArrival(qr_code); // get most data
+      const transactionObj = data.rows[0];
 
-  // vehicles
-    // make
-    // color
-    // license plate
+      // create query that returns smallest parking spot number in given transactions garage
+      const parkingSpot = await model.querySmallestParkingSpot(qr_code);
+      const ps_id = parkingSpot.rows[0].id;
+      const position = Number(parkingSpot.rows[0].position);
 
-  // garages
-    // garage address
+      // combine
+      transactionObj.parking_spot_number = position;
 
-  // parking_spots
-    // parking spot
+      console.log({transactionObj});
 
-  res.status(201).send('test');
+      await model.updateReservationCheckIn(qr_code, ps_id);
+      await model.updateParkingSpotStatusCheckIn(ps_id);
+      if (data.rows.length > 0) {
+        res.status(201).send(transactionObj);
+      } else {
+        res.status(404).send('No data found for the provided QR code.');
+      }
 
-  // also need to update values
+    } else if (current_status === 'checked-in') {
+      console.log('checking out');
+      // update values and return ps_id
+      const parking_spot = await model.updateReservationCheckOut(qr_code);
+      const ps_id = parking_spot.rows[0].parking_spot_id;
+      await model.updateParkingSpotStatusCheckOut(ps_id);
+      res.status(201).send('Successfully updated transaction.');
+
+    } else {
+      res.status(404).send('Transaction is no longer active.');
+    }
+
+  } catch (err) {
+    console.log('an error occurred on transaction/:qr_code route', err);
+    res.status(500).send('Internal Server Error');
+  }
+
 });
 
 module.exports = transactionRouter;
