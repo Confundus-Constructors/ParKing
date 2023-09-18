826f8f4d0e3fab1462d45b895e9331a31ba75ed5
Kurt Vardeman <kurtvardeman@gmail.com>
2023-09-18 11:15:30 -0400

    changed datatypes for qr_code in database

--- server/index.js
+++ server/index.js
@@ -2,19 +2,21 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+
 require("dotenv").config();
 // const userRoute = require('./routes/users');
 // const restRouter = require('./routes/transactions.js');
 const garageRouter = require("./routes/garages.js");
 const vehicleRouter = require("./routes/vehicles.js");
 const { getUser, postUser, getAll } = require("./routes/users");
+const transactionRouter = require("./routes/transactions.js");
 
 // app.use(express.static(path.join(__dirname, "../public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use('/users', userRoute);
-// app.use('/transactions', transactionsRoute);
+app.use("/transactions", transactionRouter);
 app.use("/garages", garageRouter);
 app.use("/vehicles", vehicleRouter);
 
--- server/models/index.js
+++ server/models/index.js
@@ -23,21 +23,30 @@ module.exports = {
   queryCountReservationTimes: (time1, time2) => {
     return client.query(
       `SELECT CAST(COUNT(id) AS INT), garage_id
-    FROM transactions
-    WHERE
-    (reservation_end_time >= '${time1}' AND reservation_end_time <= '${time2}')
-    OR
-    (reservation_start_time >= '${time1}' AND reservation_start_time <= '${time2}')
-    OR
-    (reservation_start_time <= '${time1}' AND reservation_end_time >= '${time2}')
-    GROUP BY garage_id`
+      FROM transactions
+      WHERE
+      (reservation_end_time >= '${time1}' AND reservation_end_time <= '${time2}')
+      OR
+      (reservation_start_time >= '${time1}' AND reservation_start_time <= '${time2}')
+      OR
+      (reservation_start_time <= '${time1}' AND reservation_end_time >= '${time2}')
+      GROUP BY garage_id`
     );
   },
   queryCountParkingSpots: () => {
     return client.query(
       `SELECT CAST(COUNT(id) AS INT), garage_id
-    FROM parking_spots
-    GROUP BY garage_id`
+      FROM parking_spots
+      GROUP BY garage_id`
+    );
+  },
+  queryReservationUponArrival: (conf_number) => {
+    return client.query(
+      `SELECT qr_code, reservation_start_time, reservation_end_time, ps.position
+      FROM transactions ts
+      INNER JOIN parking_spots ps
+      ON ps.id = ts.parking_spot_id
+      WHERE qr_code = '${conf_number}'`
     );
   },
 };
