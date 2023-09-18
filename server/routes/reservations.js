fdb3e9793e7639027083204bcd0c7889d638d3e1
Kurt Vardeman <kurtvardeman@gmail.com>
2023-09-18 16:58:20 -0400

    fixed bugs with reservation routes - should work as intended now

--- server/routes/reservations.js
+++ server/routes/reservations.js
@@ -6,8 +6,13 @@ const model = require('../models');
 reservationRouter.get('/:garage_id', async (req, res) => {
   const garageId = req.params.garage_id;
   const filter = req.query.filter;
-  const reservations = await model.queryReservations(garageId, filter);
-  res.status(201).send(reservations.rows);
+  if (filter === 'reserved') {
+    const reservations = await model.queryReservations(garageId, filter);
+    res.status(201).send(reservations.rows);
+  } else {
+    const reservations = await model.queryReservationsParked(garageId, filter);
+    res.status(201).send(reservations.rows);
+  }
 });
 
 module.exports = reservationRouter;
