93a3f52c11a5e23321d26c5685deca3ddcccef8f
LeDerius Franklin <100436192+lederius@users.noreply.github.com>
2023-09-19 07:39:26 -0700

    merge conlict resolved

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const model = require('./models/index.js');
-const multer = require('multer');
-const cors = require('cors');
+const model = require("./models/index.js");
+const multer = require("multer");
+const cors = require("cors");
 // const {host, port} = require("../env.js");
 require("dotenv").config();
 // const userRoute = require('./routes/users');
@@ -13,28 +13,24 @@ const transactionRouter = require("./routes/transactions.js");
 const reservationRouter = require("./routes/reservations.js");
 const garageRouter = require("./routes/garages.js");
 const vehicleRouter = require("./routes/vehicles.js");
-const { getUser, getEmployeeGarage, postUser, putUser, getAll, putUserAuth } = require("./routes/users");
+const { getUser, postUser, getAll } = require("./routes/users");
 
 // app.use(express.static(path.join(__dirname, "../public")));
-app.use(bodyParser.json({limit: '50mb', extended: true}));
-app.use(bodyParser.urlencoded({limit: "50mb", extended: true }));
-app.use(bodyParser.text({ limit: '200mb' }));
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.text({ limit: "200mb" }));
 app.use(cors());
 // app.use('/users', userRoute);
-app.use('/reservations', reservationRouter);
-app.use('/transactions', transactionRouter);
-app.use('/garages', garageRouter);
-app.use('/vehicles', vehicleRouter);
+app.use("/reservations", reservationRouter);
+app.use("/transactions", transactionRouter);
+app.use("/garages", garageRouter);
+app.use("/vehicles", vehicleRouter);
 
 const port = process.env.SERVER_PORT;
 
 app.get("/users", (req, res) => {
   req.query.length > 0 ? getUser(req, res) : getAll(req, res);
 });
-
-app.get("/users/employees/:id", (req, res) => {
-  getEmployeeGarage(req, res);
-})
 
 app.post("/users", (req, res) => {
   postUser(req, res);
@@ -50,19 +46,19 @@ app.put("/users/auth", (req, res) => {
 
 app.post("/image", async (req, res) => {
   try {
-   const result = await model.updateCarPhoto(req.body.qr_code, req.body.image);
+    const result = await model.updateCarPhoto(req.body.qr_code, req.body.image);
     res.end("Picture Updated");
   } catch (err) {
-    res.status(404).send('Error while updating picture');
+    res.status(404).send("Error while updating picture");
   }
 });
 
-app.get("/image/:qr_code", async(req, res) => {
+app.get("/image/:qr_code", async (req, res) => {
   try {
     const result = await model.getCarPhoto(req.params.qr_code);
-    res.json(result)
+    res.json(result);
   } catch (err) {
-    res.status(404).send('Error while getting picture');
+    res.status(404).send("Error while getting picture");
   }
 });
 
