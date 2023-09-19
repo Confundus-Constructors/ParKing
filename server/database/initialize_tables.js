a45ff730d352dd107bfb27be4691003f21517d1e
Kurt Vardeman <kurtvardeman@gmail.com>
2023-09-19 11:44:23 -0400

    fixed issue with tables not auto incrementing

--- server/database/initialize_tables.js
+++ server/database/initialize_tables.js
@@ -1,5 +1,6 @@
 const client = require('./db.js');
 const query = require('./initialize_tables_query.js');
+const seq_query = require('./alter_sequence_query.js');
 const usersData = require('../../exampleData/users');
 const valetCompanyData = require('../../exampleData/valet_companies');
 const garagesData = require('../../exampleData/garages');
@@ -7,6 +8,22 @@ const parkingSpotsData = require('../../exampleData/parking_spots');
 const employeesData = require('../../exampleData/employees');
 const vehiclesData = require('../../exampleData/vehicles');
 const transactionsData = require('../../exampleData/transactions');
+
+
+alterStatementsArray = [
+  "SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));",
+  "SELECT setval('vehicles_id_seq', (SELECT MAX(id) FROM vehicles));",
+  "SELECT setval('garages_id_seq', (SELECT MAX(id) FROM garages));",
+  "SELECT setval('parking_spots_id_seq', (SELECT MAX(id) FROM parking_spots));",
+  "SELECT setval('employees_id_seq', (SELECT MAX(id) FROM employees));",
+  "SELECT setval('transactions_id_seq', (SELECT MAX(id) FROM transactions));",
+  // `ALTER SEQUENCE "vehicles_id_seq" RESTART WITH (SELECT MAX(id) + 1 FROM "vehicles");`,
+  // `ALTER SEQUENCE "valet_company_id_seq" RESTART WITH (SELECT MAX(id) + 1 FROM "valet_company");`,
+  // `ALTER SEQUENCE "garages_id_seq" RESTART WITH (SELECT MAX(id) + 1 FROM "garages");`,
+  // `ALTER SEQUENCE "parking_spots_id_seq" RESTART WITH (SELECT MAX(id) + 1 FROM "parking_spots");`,
+  // `ALTER SEQUENCE "employees_id_seq" RESTART WITH (SELECT MAX(id) + 1 FROM "employees");`,
+  // `ALTER SEQUENCE "transactions_id_seq" RESTART WITH (SELECT MAX(id) + 1 FROM "transactions");`,
+];
 
 (async () => {
   try {
@@ -18,6 +35,11 @@ const transactionsData = require('../../exampleData/transactions');
     await seedTables('employees', employeesData);
     await seedTables('vehicles', vehiclesData);
     await seedTables('transactions', transactionsData);
+    for (let element of alterStatementsArray) {
+      element.toString();
+      console.log(element);
+      await client.query(element);
+    }
     await client.end();
   } catch (err) {
     console.error(err);
