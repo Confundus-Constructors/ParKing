906aef96da0d2960051d8399fb91130123db2d73
Aslambek Baimouradov <zandak@gmail.com>
2023-09-19 10:49:42 -0400

    rebasedone

--- App.js
+++ App.js
@@ -60,7 +60,7 @@ export default function App() {
     <Navigation />
     // <Select />
     // <UHP />
-    <Welcome />
+    // <Welcome />
     // <SignUpScreen />
     <CarManage />
     // <Reserve />
