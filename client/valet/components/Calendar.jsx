1bce3443abbadeca7bccfdcc9f2c2d315ae89fc3
mcuellar98 <mcuellar228@gmail.com>
2023-09-20 17:19:19 -0400

    Fixed nest navigation container problem. Added time picker

--- client/valet/components/Calendar.jsx
+++ client/valet/components/Calendar.jsx
@@ -1,8 +1,22 @@
 
 import {View } from 'react-native';
+import DateTimePicker from "@react-native-community/datetimepicker";
+import {useState} from 'react';
 
 export default Calendar = () => {
+
+  const [sDate, setSDate] = useState(new Date());
+  const [eDate, setEDate] = useState(new Date());
+  const [sTime, setSTime] = useState(new Date());
+  const [eTime, setETime] = useState(new Date());
+
   return (
-    <View/>
+    <DateTimePicker
+              testID="dateTimePicker"
+              value={eTime}
+              mode="time"
+              is24Hour={false}
+              // onChange={handleEndTime}
+            />
   )
 }
