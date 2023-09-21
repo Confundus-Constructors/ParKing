6bb255834314203ccb8c9698bb19f4ec8ef6c62b
Kurt Vardeman <kurtvardeman@gmail.com>
2023-09-20 20:07:53 -0400

    fixed issues with svg, and other libraries

--- client/user/navigation/index.js
+++ client/user/navigation/index.js
@@ -18,7 +18,7 @@ const Navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {/* <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Welcome" component={Welcome} />
         <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
         <Stack.Screen
           name="ConfirmEmailScreen"
@@ -28,7 +28,7 @@ const Navigation = () => {
           name="ForgotPasswordScreen"
           component={ForgotPasswordScreen}
         />
-        <Stack.Screen name="NewPasswordScreen" component={NewPasswordScreen} /> */}
+        <Stack.Screen name="NewPasswordScreen" component={NewPasswordScreen} />
         <Stack.Screen name="UHP" component={UHP} />
         <Stack.Screen name="Reserve" component={Reserve} />
         <Stack.Screen name="Select" component={Select} />
