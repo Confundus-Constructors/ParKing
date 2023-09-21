0fcbfb788a21a3fc8917f97695cd5ceb7c94d37b
Jon Williams <jonwill04@icloud.com>
2023-09-21 17:29:51 -0400

    asdsa

--- client/valet/components/CarCard.jsx
+++ client/valet/components/CarCard.jsx
@@ -1,14 +1,20 @@
-import React, {useState, useEffect} from 'react';
-import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity, Image, Button} from 'react-native';
-import {launchCamera} from 'react-native-image-picker';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faCamera } from '@fortawesome/free-solid-svg-icons';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Button,
+} from "react-native";
+import { launchCamera } from "react-native-image-picker";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
-
-
-const CarCard = ({info,  buttonText, navigation}) => {
-  console.log('carcard log',  navigation)
+const CarCard = ({ info, buttonText, navigation }) => {
+  console.log("carcard log", navigation);
   const [imageSource, setImageSource] = useState(null);
 
   const formatCustomDate = (date) => {
@@ -16,10 +22,12 @@ const CarCard = ({info,  buttonText, navigation}) => {
     const month = date.getMonth() + 1;
     const year = date.getFullYear().toString().slice(2);
     const hours = date.getHours();
-    const minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
-    const ampm = hours >= 12 ? 'PM' : 'AM';
+    const minutes = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+    const ampm = hours >= 12 ? "PM" : "AM";
 
-    const formattedDate = `${month}/${day}/${year}, ${hours % 12 || 12}:${minutes} ${ampm}`;
+    const formattedDate = `${month}/${day}/${year}, ${
+      hours % 12 || 12
+    }:${minutes} ${ampm}`;
     return formattedDate;
   };
 
@@ -27,33 +35,32 @@ const CarCard = ({info,  buttonText, navigation}) => {
   const date2 = new Date(info.reservation_end_time);
 
   useEffect(() => {
-    axios.get(`http://localhost:3000/image/${info.confirmation_id}`)
-    .then((result) => {
-      if (result.data.rows[0].photo) {
-        var base64 = result.data.rows[0].photo;
-        var base64Pic = 'data:image/png;base64,' + base64;
-        setImageSource(base64Pic)
-      }
-    })
+    axios
+      .get(`http://localhost:3000/image/${info.confirmation_id}`)
+      .then((result) => {
+        if (result.data.rows[0].photo) {
+          var base64 = result.data.rows[0].photo;
+          var base64Pic = "data:image/png;base64," + base64;
+          setImageSource(base64Pic);
+        }
+      });
   });
 
   const selectImage = () => {
     const options = {
-      mediaType: 'photo',
+      mediaType: "photo",
       includeBase64: false,
       maxHeight: 200,
       maxWidth: 200,
     };
 
-
-
     launchCamera(options, (response) => {
       if (response.didCancel) {
-        console.log('Camera cancelled');
+        console.log("Camera cancelled");
       } else if (response.error) {
-        console.log('ImagePicker Error:', response.error);
+        console.log("ImagePicker Error:", response.error);
       } else {
-        const source = {uri: response.uri};
+        const source = { uri: response.uri };
         setImageSource(source);
       }
     });
@@ -61,24 +68,32 @@ const CarCard = ({info,  buttonText, navigation}) => {
 
   const capitalizeString = (string) => {
     if (string) {
-      return string[0].toUpperCase() + string.slice(1,string.length).toLowerCase();
-    }
-    else {
+      return (
+        string[0].toUpperCase() + string.slice(1, string.length).toLowerCase()
+      );
+    } else {
       return null;
     }
   };
 
   const handleCheckCar = () => {
     if (buttonText) {
-      navigation.navigate('CheckIn', {qr_code: info.confirmation_id, carInfo: info, list: true})
+      navigation.navigate("CheckIn", {
+        qr_code: info.confirmation_id,
+        carInfo: info,
+        list: true,
+      });
     } else {
-      navigation.navigate('CheckOut', {qr_code: info.confirmation_id, carInfo: info, list: true})
+      navigation.navigate("CheckOut", {
+        qr_code: info.confirmation_id,
+        carInfo: info,
+        list: true,
+      });
     }
-  }
+  };
 
   return (
     <SafeAreaView className="text-lg" style={styles.container}>
-
       <View style={styles.row}>
         <View style={styles.row}>
           <View>
@@ -109,75 +124,82 @@ const CarCard = ({info,  buttonText, navigation}) => {
             {imageSource ? (
               <Image src={imageSource} style={styles.image} />
             ) : (
-              <FontAwesomeIcon icon={faCamera} style={{color: "#a9927d"}} size={80} fade-size={'lg'}/>
-
+              <FontAwesomeIcon
+                icon={faCamera}
+                style={{ color: "#a9927d" }}
+                size={80}
+                fade-size={"lg"}
+              />
             )}
           </TouchableOpacity>
-      </View>
-
-      <View style={styles.row}>
-        <View>
-        <Text style={styles.row}>
-          <Text style={styles.boldText}>Arrival: </Text>
-          <Text style={styles.user}>{formatCustomDate(date1)}</Text>        </Text>
-        <Text style={styles.row}>
-          <Text style={styles.boldText}>Depart: </Text>
-          <Text style={styles.user}>{formatCustomDate(date2)}</Text>        </Text>
         </View>
-        <View>
-        <Text style={styles.row}>
-          <Text style={styles.boldText}>Garage: </Text>
-          <Text style={styles.user}>{}</Text>
-        </Text>
-        <Text style={styles.row, {paddingRight: 60}}>
-          <Text style={styles.boldText}>Spot ID: </Text>
-          <Text>{info.parking_spot_number}</Text>
-        </Text>
+
+        <View style={styles.row}>
+          <View>
+            <Text style={styles.row}>
+              <Text style={styles.boldText}>Arrival: </Text>
+              <Text style={styles.user}>{formatCustomDate(date1)}</Text>{" "}
+            </Text>
+            <Text style={styles.row}>
+              <Text style={styles.boldText}>Depart: </Text>
+              <Text style={styles.user}>{formatCustomDate(date2)}</Text>{" "}
+            </Text>
+          </View>
+          <View>
+            <Text style={styles.row}>
+              <Text style={styles.boldText}>Garage: </Text>
+              <Text style={styles.user}>{}</Text>
+            </Text>
+            <Text style={(styles.row, { paddingRight: 60 })}>
+              <Text style={styles.boldText}>Spot ID: </Text>
+              <Text>{info.parking_spot_number}</Text>
+            </Text>
+          </View>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={handleCheckCar}>
+            <Text style={styles.buttonText}>
+              {buttonText ? buttonText : "Check Out"}
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
-    <View style={styles.buttonContainer}>
-      <TouchableOpacity style={styles.button} onPress={handleCheckCar}>
-      <Text style={styles.buttonText}>{buttonText ? buttonText : 'Check Out'}</Text>
-      </TouchableOpacity>
-    </View>
-  </SafeAreaView>
-
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#D6D6D6',
+    backgroundColor: "#D6D6D6",
     padding: 10,
     borderRadius: 0,
     marginBottom: 10,
     borderRadius: 10,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 3 },
     shadowOpacity: 0.3,
     shadowRadius: 2,
 
     // Android shadow style
-    elevation: 5
+    elevation: 5,
   },
   row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginTop: 2,
     marginLeft: 1,
     fontSize: 16,
   },
   boldText: {
-    fontWeight: 'bold',
-    color: 'black',
-
+    fontWeight: "bold",
+    color: "black",
   },
   box: {
     width: 180,
     height: 120,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
     borderRadius: 10,
     marginRight: 5,
   },
@@ -222,36 +244,36 @@ const styles = StyleSheet.create({
     marginTop: 15,
   },
   buttonContainer: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   button: {
-    backgroundColor: '#49111c',
+    backgroundColor: "#49111c",
     padding: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     borderRadius: 10,
-    width: '98%',
+    width: "98%",
     marginBottom: 5,
     marginTop: 5,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.7,
     shadowRadius: 2,
 
     // Android shadow style
-    elevation: 5
+    elevation: 5,
   },
   buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
     fontSize: 16,
-},
+  },
   user: {
     marginTop: 1,
     fontSize: 16,
-    fontWeight: 'bold',
-    color: '#404040',
-  }
+    fontWeight: "bold",
+    color: "#404040",
+  },
 });
 
 export default CarCard;
