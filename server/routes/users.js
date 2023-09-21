35e5a8bb83c9ef51e58140773cdfe15d99c27107
Kurt Vardeman <kurtvardeman@gmail.com>
2023-09-21 11:01:30 -0400

    updated users routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,19 +5,6 @@ module.exports = {
   getAll: (req, res) => {
     queryAll("users");
   },
-  getEmployeeGarage: (req, res) => {
-    const user_id = req.params.id;
-    console.log('params: ', req.params);
-    console.log(user_id);
-    client.query(`SELECT garage_id from employees WHERE user_id = $1`, [user_id])
-      .then((data) => {
-        console.log('success');
-        res.status(201).send(data.rows[0]);
-      })
-      .catch((err) =>{
-        res.status(201).send([]);
-      })
-  },
   getUser: (req, res) => {
     let { email, password } = req.query;
     console.log(email, password);
@@ -64,39 +51,13 @@ module.exports = {
         `UPDATE users
         SET device_token = $1
         WHERE email = $2
-        AND password = $3
+        and password = $3
         RETURNING id, is_employee;
         `,
         [device_token, email, password]
       )
         .then((query) => {
           // console.log(query.rows);
-          res.status(201).send(query.rows[0]);
-        })
-        .catch((err) => {
-          console.log(err);
-          res.status(404).send(err);
-        });
-  },
-  putUserAuth: async (req, res) => {
-    let { email, /*password,*/ stsTokenManager } = req.body;
-    console.log(email);
-    const device_token = stsTokenManager.accessToken;
-    // console.log({email, password, device_token});
-
-    client
-      .query(
-        `UPDATE users
-        SET device_token = $1
-        WHERE email = $2
-        /*and password = $3*/
-        RETURNING id, is_employee;
-        `,
-        [device_token, email/*, password*/]
-      )
-        .then((query) => {
-          // console.log(query.rows);
-          console.log(query.rows);
           res.status(201).send(query.rows[0]);
         })
         .catch((err) => {
