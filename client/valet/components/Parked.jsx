cf4c0fa138fcb5a1f9fcad81207557223551c5cc
mcuellar98 <mcuellar228@gmail.com>
2023-09-21 10:23:24 -0400

    Added keys to car cards

--- client/valet/components/Parked.jsx
+++ client/valet/components/Parked.jsx
@@ -1,59 +1,31 @@
-import React, {useState, useEffect, useContext} from 'react';
-import { View, ScrollView } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View } from 'react-native';
 import axios from 'axios';
 import CarCard from './CarCard.jsx';
-import {host, port} from "../../../env.js";
-import { RefreshContext } from './CarManage.jsx';
-import {Text, StyleSheet} from 'react-native';
 
 
-const Parked = ({garage = 1, navigation}) => {
-  const refreshKey = useContext(RefreshContext);
+const Parked = ({garage = 1}) => {
   const [resInfo, setResInfo] = useState([])
 
   useEffect (() => {
-    axios.get(`http://${host}:${port}/reservations/${garage}`, {
+    axios.get(`http://localhost:3000/reservations/${garage}`, {
       params: {
         filter: 'checked-in'
       }
     })
     .then(res => {
-      // console.log('res log', res.data)
+      console.log('res log', res.data)
       setResInfo(res.data)
     })
-    .then((result) => {
-      console.log(result.data);
-    })
     .catch(err => console.log('Error fetching reserved', err))
-  }, [refreshKey])
+  }, [garage])
 
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-      {resInfo.length === 0 ?
-       <View style={styles.container}>
-         <Text style={styles.nores}>No Cars Parked</Text>
-       </View>
-         :
-        <View>
-         {resInfo.map(res => <CarCard key={res.confirmation_id} navigation={navigation} garage={garage} info={res}/>)}
-        </View>
-     }
-
-    </ScrollView>
-
+    <View>
+      {resInfo.map(res => <CarCard key={res.confirmation_id} info={res}/>)}
+    </View>
   )
+
 }
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100%',
-    },
-    nores: {
-      color: '#49111c',
-      fontWeight: 'bold',
-      fontSize: 20,
-    }})
 
 export default Parked;
--- client/valet/components/Reserved.jsx
+++ client/valet/components/Reserved.jsx
@@ -1,55 +1,31 @@
-import React, {useState, useEffect, useContext} from 'react';
-import { View, ScrollView } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View } from 'react-native';
 import axios from 'axios';
 import CarCard from './CarCard.jsx';
-import {host, port} from "../../../env.js";
-import { RefreshContext } from './CarManage.jsx';
-import {Text, StyleSheet} from 'react-native';
 
-
-const Reserved = ({garage = 2, navigation}) => {
-  const refreshKey = useContext(RefreshContext);
+const Reserved = ({garage = 1, navigation}) => {
   const [resInfo, setResInfo] = useState([])
   const buttontext = 'Check In';
+
   useEffect (() => {
-    axios.get(`http://${host}:${port}/reservations/${garage}`, {
+    axios.get(`http://localhost:3000/reservations/${garage}`, {
       params: {
         filter: 'reserved'
       }
     })
     .then(res => {
-      console.log('logger', res.data)
+      console.log('res log', res.data)
+      setResInfo(res.data)
     })
     .catch(err => console.log('Error fetching reserved', err))
-  }, [refreshKey])
+  }, [garage])
 
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-      {resInfo.length === 0 ?
-        <View style={styles.container}>
-          <Text style={styles.nores}>No Reservations</Text>
-        </View>
-         :
-        <View>
-         {resInfo.map(res => <CarCard key={res.confirmation_id} navigation={navigation} info={res} garage={garage} buttonText={buttontext}/>)}
-      </View>
-      }
-    </ScrollView>
+    <View>
+      {resInfo.map(res => <CarCard key={res.confirmation_id} navigation={navigation} info={res} buttonText={buttontext}/>)}
+    </View>
   )
+
 }
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100%',
-
-    },
-    nores: {
-      color: '#49111c',
-      fontWeight: 'bold',
-      fontSize: 20,
-    }})
-
 
 export default Reserved;
