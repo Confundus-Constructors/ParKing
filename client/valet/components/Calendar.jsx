3002233e6b5e04dc6b4c6b39ea7f8f127174bba4
mcuellar98 <mcuellar228@gmail.com>
2023-09-20 20:10:03 -0400

    Fixed image uploading bug and added calendar

--- client/valet/components/Calendar.jsx
+++ client/valet/components/Calendar.jsx
@@ -1,22 +1,33 @@
 
-import {View } from 'react-native';
-import DateTimePicker from "@react-native-community/datetimepicker";
+import {View, Text, SafeAreaView} from 'react-native';
+// import DateTimePicker from "@react-native-community/datetimepicker";
 import {useState} from 'react';
+import {Calendar, LocaleConfig} from 'react-native-calendars';
 
-export default Calendar = () => {
+export default ValetCalendar = () => {
 
-  const [sDate, setSDate] = useState(new Date());
-  const [eDate, setEDate] = useState(new Date());
-  const [sTime, setSTime] = useState(new Date());
-  const [eTime, setETime] = useState(new Date());
+  // const [sDate, setSDate] = useState(new Date());
+  // const [eDate, setEDate] = useState(new Date());
+  // const [sTime, setSTime] = useState(new Date());
+  // const [eTime, setETime] = useState(new Date());
 
   return (
-    <DateTimePicker
-              testID="dateTimePicker"
-              value={eTime}
-              mode="time"
-              is24Hour={false}
-              // onChange={handleEndTime}
-            />
+    <Calendar
+      style={{
+        borderWidth: 1,
+        borderColor: 'gray',
+        height: 350,
+      }}
+      theme={{
+        backgroundColor: '#ffffff',
+        calendarBackground: '#ffffff',
+        textSectionTitleColor: '#b6c1cd',
+        selectedDayBackgroundColor: '#00adf5',
+        selectedDayTextColor: '#ffffff',
+        todayTextColor: '#00adf5',
+        dayTextColor: '#2d4150',
+        textDisabledColor: '#d9e'
+      }}
+      />
   )
 }
