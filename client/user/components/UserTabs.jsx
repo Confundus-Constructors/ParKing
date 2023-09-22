e2f9d7ce677aa042495a649b613722ac8b3f5aba
Jon Williams <jonwill04@icloud.com>
2023-09-22 16:35:56 -0400

    Revert "merge"
    This reverts commit 0edbd25b4e01fb8f295bd4ef6bf2c091cb649691.

--- client/user/components/UserTabs.jsx
+++ client/user/components/UserTabs.jsx
@@ -1,26 +1,34 @@
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
-import { useTheme } from "react-native-paper";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import Reservations from "./Reservations.jsx";
-import Welcome from "./Welcome.jsx";
-import { FIREBASE_AUTH } from "../../../FirebaseConfig.ts";
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import { useTheme } from 'react-native-paper';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Reservations from './Reservations.jsx';
+import Welcome from './Welcome.jsx';
+import { FIREBASE_AUTH } from '../../../FirebaseConfig.ts';
 import { signOut } from "firebase/auth";
-import MapScreens from "./MapScreens";
-import HomePageScreens from "./HomePageScreens";
-import { useRoute } from "@react-navigation/native";
-import SignOutScreen from "./SignOutScreen";
+import MapScreens from './MapScreens';
+import HomePageScreens from './HomePageScreens';
+import { useRoute } from '@react-navigation/native';
+import SignOutScreen from './SignOutScreen';
+import {useState, useEffect} from 'react';
 
 export default UserTabs = () => {
   const route = useRoute();
   const theme = useTheme();
-  const userId = route.params.data;
+  const [userId,setId] = useState(16);
   theme.colors.secondaryContainer = "transparent";
+
+  useEffect(() => {
+    if (route.params) {
+      console.log('this is the user id', route.params.data)
+      setId(route.params.data);
+    }
+  },[])
 
   const navTheme = {
     colors: {
-      background: "white",
-    },
+      background: "white"
+    }
   };
 
   const Tab = createMaterialBottomTabNavigator();
@@ -33,48 +41,48 @@ export default UserTabs = () => {
   };
   return (
     // <NavigationContainer theme={navTheme}>
-    <Tab.Navigator
-      barStyle={{ backgroundColor: "black" }}
+      <Tab.Navigator
+      barStyle={{ backgroundColor: 'black' }}
       activeColor="white"
       inactiveColor="grey"
-      // inactiveTintColor='grey'
-      // activeTintColor= 'red'
-    >
+        // inactiveTintColor='grey'
+        // activeTintColor= 'red'
+      >
       <Tab.Screen
-        name="Home"
-        initialParams={{ id: userId }}
-        component={HomePageScreens}
-        options={{
-          tabBarLabel: "Home",
-          barTintColor: "white",
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="home" color={color} size={32} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Map"
-        component={MapScreens}
-        options={{
-          tabBarLabel: "Map",
-          barTintColor: "white",
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="map" color={color} size={32} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Sign Out Screen"
-        component={SignOutScreen}
-        options={{
-          tabBarLabel: "Sign Out",
-          barTintColor: "white",
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="exit-run" color={color} size={32} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+          name="Home"
+          initialParams={{id: userId}}
+          component={HomePageScreens}
+          options={{
+            tabBarLabel: 'Home',
+            barTintColor: 'white',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={32} />
+            )
+          }}
+        />
+        <Tab.Screen
+          name="Map"
+          component={MapScreens}
+          options={{
+            tabBarLabel: 'Map',
+            barTintColor: 'white',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="map" color={color} size={32} />
+            )
+          }}
+        />
+        <Tab.Screen
+          name="Sign Out Screen"
+          component={SignOutScreen}
+          options={{
+            tabBarLabel: 'Sign Out',
+            barTintColor: 'white',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="exit-run" color={color} size={32} />
+            )
+          }}
+        />
+      </Tab.Navigator>
     // </NavigationContainer>
-  );
+  )
 };
