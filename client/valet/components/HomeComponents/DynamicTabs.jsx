45ee2ddb9f53b8ddd9a41a99233602bf1dce5a52
Aslambek Baimouradov <zandak@gmail.com>
2023-10-06 11:53:44 -0400

    tabs finally working

--- client/valet/components/HomeComponents/DynamicTabs.jsx
+++ client/valet/components/HomeComponents/DynamicTabs.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView } from 'react-native';
-import { NavigationContainer, useNavigation} from '@react-navigation/native';
+import { SafeAreaView, Text, TouchableOpacity } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { useNavigation } from '@react-navigation/native';
 import ValetInfo from './ValetInfo';
 import SignOut from '../SignOut';
 import { BlurView } from '@react-native-community/blur';
-import { Button, Text, View, Modal, TouchableOpacity } from 'react-native';
+import { Button, View, Modal } from 'react-native';
 import Why from './Why';
 import UploadID from './UploadID';
 import Payment from './Payment';
@@ -14,78 +14,94 @@ import LocTab from './LocTab';
 
 const Tab = createMaterialTopTabNavigator();
 
-function HomeContent({ navigation }) {
-    const [seeModal, setSeeModal] = useState(false);
-    const [showBlur, setShowBlur] = useState(false);
+function HomeContent() {
+  const navigation = useNavigation();
+  const [seeModal, setSeeModal] = useState(false);
+  const [showBlur, setShowBlur] = useState(false);
 
-    const handleEdit = () => {
-        setSeeModal(!seeModal);
-        setShowBlur(!showBlur);
-    }
+  const handleEdit = () => {
+    setSeeModal(!seeModal);
+    setShowBlur(!showBlur);
+  };
 
-    const blur = () => {
-        setShowBlur(!showBlur);
-    }
+  const blur = () => {
+    setShowBlur(!showBlur);
+  };
 
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <View style={styles.container}>
-                <ValetInfo navigation={navigation} blur={blur}/>
-                <View style={styles.signout}>
-                    <SignOut variant={'home'} />
-                </View>
-            </View>
-            <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-                <Text style={styles.title}>Upload documentation to get started  -  </Text>
-                <TouchableOpacity onPress={handleEdit}>
-                    <Text style={styles.editText}>Why?</Text>
-                </TouchableOpacity>
-            </View>
-            <View style={{marginTop: -10}}>
-                <UploadID navigation={navigation}/>
-            </View>
-            <View style={{ flexDirection: 'column', alignItems: 'center' }}>
-                <Payment />
-                <Text style={styles.subtitle}>You currently have:</Text>
-                <Summary />
-            </View>
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <ValetInfo blur={blur} />
+        <View style={styles.signout}>
+          <SignOut variant={'home'} />
+        </View>
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+        <Text style={styles.title}>Upload documentation to get started - </Text>
+        <TouchableOpacity onPress={handleEdit}>
+          <Text style={styles.editText}>Why?</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{ marginTop: -10 }}>
+        <UploadID />
+      </View>
+      <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+        <Payment />
+        <Text style={styles.subtitle}>You currently have:</Text>
+        <Summary />
+      </View>
 
-            {showBlur && (
-                <BlurView
-                style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}
-                blurType="light"
-                blurAmount={2}
-                reducedTransparencyFallbackColor="white"
-                />
-            )}
+      {showBlur && (
+        <BlurView
+          style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}
+          blurType="light"
+          blurAmount={2}
+          reducedTransparencyFallbackColor="white"
+        />
+      )}
 
-            <Modal
-                animationType='slide'
-                transparent={true}
-                visible={seeModal}
-                onRequestClose={() => {
-                    setSeeModal(!seeModal);
-                }}
-            >
-                <SafeAreaView style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center', marginTop: 110 }}>
-                    <View style={{ width: '95%', height: 540, backgroundColor: '#F7F7F7', padding: 10, borderRadius: 20 }}>
-                        <Why />
-                        <Button title="OK" onPress={handleEdit} />
-                    </View>
-                </SafeAreaView>
-            </Modal>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={seeModal}
+        onRequestClose={() => {
+          setSeeModal(!seeModal);
+        }}
+      >
+        <SafeAreaView
+          style={{
+            flex: 1,
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            marginTop: 110,
+          }}
+        >
+          <View
+            style={{
+              width: '95%',
+              height: 540,
+              backgroundColor: '#F7F7F7',
+              padding: 10,
+              borderRadius: 20,
+            }}
+          >
+            <Why />
+            <Button title="OK" onPress={handleEdit} />
+          </View>
         </SafeAreaView>
-    );
+      </Modal>
+    </SafeAreaView>
+  );
 }
 
-const DynamicTabs = ({navigation}) => {
+const DynamicTabs = ({ navigation }) => {
   const [userTabs, setUserTabs] = useState([]);
 
   const addNewTab = () => {
     const newTabIndex = userTabs.length + 1;
     const newTab = {
-        name: `Location ${newTabIndex}`,
-        component: LocTab
+      name: `Location ${newTabIndex}`,
+      component: LocTab,
     };
     setUserTabs([...userTabs, newTab]);
   };
@@ -95,74 +111,76 @@ const DynamicTabs = ({navigation}) => {
       const latestTab = userTabs[userTabs.length - 1];
       navigation.navigate(latestTab.name);
     }
-  }, [userTabs]);
+  }, [userTabs, navigation]);
 
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-                <Tab.Navigator
-                    tabBarOptions={{
-                        activeTintColor: 'white',
-                        inactiveTintColor: '#49111c',
-                        labelStyle: { fontSize: 16 },
-                        style: { backgroundColor: '#a9927d', marginBottom: 3 },
-                        indicatorStyle: { backgroundColor: 'white' },
-                        scrollEnabled: true,
-                    }}
-                 >
-                      <Tab.Screen
-                          name="Account"
-                          component={HomeContent}
-                          options={{
-                              tabBarLabel: ({ focused, color, position }) => (
-                                  <Text style={{ fontSize: 20, fontWeight: 'bold', color: color }}>
-                                      Account
-                                  </Text>
-                              )
-                          }}
-                      />
-                    {userTabs.map((tab, index) => (
-                        <Tab.Screen key={index} name={tab.name} component={tab.component} />
-                    ))}
-                     <Tab.Screen name="+ Add Location" listeners={{
-                          tabPress: e => {
-                              // e.preventDefault();
-                              addNewTab();
-                          }
-                      }}>
-                          {() => null}
-                      </Tab.Screen>
-
-                </Tab.Navigator>
-        </SafeAreaView>
-    );
-}
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: 'white',
+          inactiveTintColor: '#49111c',
+          labelStyle: { fontSize: 16 },
+          style: { backgroundColor: '#a9927d', marginBottom: 3 },
+          indicatorStyle: { backgroundColor: 'white' },
+          scrollEnabled: true,
+        }}
+      >
+        <Tab.Screen
+          name="Account"
+          component={HomeContent}
+          options={{
+            tabBarLabel: ({ focused, color, position }) => (
+              <Text style={{ fontSize: 20, fontWeight: 'bold', color: color }}>
+                Account
+              </Text>
+            ),
+          }}
+        />
+        {userTabs.map((tab, index) => (
+          <Tab.Screen key={index} name={tab.name} component={tab.component} />
+        ))}
+        <Tab.Screen
+          name="+ Add Location"
+          listeners={{
+            tabPress: (e) => {
+              addNewTab();
+              e.preventDefault();
+            },
+          }}
+        >
+          {() => null}
+        </Tab.Screen>
+      </Tab.Navigator>
+    </SafeAreaView>
+  );
+};
 
 const styles = {
-    container: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    signout: {
-        alignItems: 'flex-start',
-    },
-    editText: {
-        fontWeight: 'bold',
-        color: '#a9927d',
-        fontSize: 15,
-        marginTop: 25,
-    },
-    title: {
-        marginTop: 25,
-        fontWeight: 'bold',
-        fontSize: 15,
-        marginBottom: 5,
-    },
-    subtitle: {
-        marginTop: 25,
-        fontWeight: 'bold',
-        fontSize: 18,
-        marginBottom: 10,
-    }
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  signout: {
+    alignItems: 'flex-start',
+  },
+  editText: {
+    fontWeight: 'bold',
+    color: '#a9927d',
+    fontSize: 15,
+    marginTop: 25,
+  },
+  title: {
+    marginTop: 25,
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginBottom: 5,
+  },
+  subtitle: {
+    marginTop: 25,
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 10,
+  },
 };
 
 export default DynamicTabs;
--- client/valet/components/HomeComponents/LocTab.jsx
+++ client/valet/components/HomeComponents/LocTab.jsx
@@ -1,21 +1,89 @@
-import React from 'react';
-import { View } from 'react-native';
-import MapView from 'react-native-maps';
+import React, { useState } from 'react';
+import { View, StyleSheet, TextInput, Button } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 function MyMap() {
+  const [address, setAddress] = useState(''); // State to store the user's input address
+  const [coordinates, setCoordinates] = useState({
+    latitude: 40.7128,
+    longitude: -74.0060,
+  }); // State to store the coordinates of the geocoded address
+
+  const handleGeocode = async () => {
+    try {
+      // Encode the user's input address to be used in the query
+      const encodedAddress = encodeURIComponent(address);
+
+      // Define the API endpoint URL with the address query parameter
+      const apiUrl = `https://maps-api.apple.com/v1/geocode?q=${encodedAddress}`;
+
+      // Make a GET request to the Apple Maps API
+      const response = await fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkRXM1JBNlE4UzMifQ.eyJpc3MiOiJUOVE5OTVGN1dLIiwiaWF0IjoxNjk2NjA1MjYxLCJleHAiOjE3MjgwMDAwMDB9.EJxpGWWxp3TGKP11u9SJ5d2KyfNGJ4J07IP8tKZ9dD_DhCc1IunM8jF2y1AdQ_1pQSxpWtyWL_SpRVcjWeeuDQ`, // Replace with your Apple Maps API access token
+        },
+      });
+
+      // Check if the response status is OK (200)
+      if (response.status === 200) {
+        const data = await response.json();
+
+        // Extract the geocoded coordinates from the response
+        if (data.results && data.results[0] && data.results[0].location) {
+          const { latitude, longitude } = data.results[0].location;
+
+          // Update the `coordinates` state with the geocoded coordinates
+          setCoordinates({ latitude, longitude });
+        } else {
+          // Handle cases where the geocoding response doesn't contain the expected data
+          console.error('Geocoding response format is not as expected.');
+        }
+      } else {
+        // Handle non-OK response statuses (e.g., error handling)
+        console.error(`Error: ${response.status}`);
+      }
+    } catch (error) {
+      // Handle any network or other errors that may occur during the request
+      console.error('An error occurred:', error);
+    }
+  };
+
+
   return (
-    <View style={{ flex: 1 }}>
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter an address"
+        value={address}
+        onChangeText={text => setAddress(text)}
       />
+      <Button title="Geocode" onPress={handleGeocode} />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          ...coordinates,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.015,
+        }}
+      >
+        <Marker coordinate={coordinates} title="Geocoded Location" />
+      </MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  map: {
+    width: 300,
+    height: 300,
+  },
+});
+
 
 export default MyMap;
