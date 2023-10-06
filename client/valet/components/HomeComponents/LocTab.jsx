2c061c6716480fc31b18ca57a92ace2707496398
Aslambek Baimouradov <zandak@gmail.com>
2023-10-05 23:28:24 -0400

    maps work

--- client/valet/components/HomeComponents/LocTab.jsx
+++ client/valet/components/HomeComponents/LocTab.jsx
@@ -1,16 +1,21 @@
-import {View, Text, SafeAreaView} from 'react-native';
 import React from 'react';
+import { View } from 'react-native';
+import MapView from 'react-native-maps';
 
-const LocTab = () => {
-
+function MyMap() {
   return (
-    <SafeAreaView>
-      <Text>
-        Howdy
-      </Text>
-    </SafeAreaView>
-
-  )
+    <View style={{ flex: 1 }}>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
+  );
 }
 
-export default LocTab;
+export default MyMap;
